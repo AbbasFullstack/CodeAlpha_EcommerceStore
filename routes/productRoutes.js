c786b0013ff4200c10d5af237508b333c1159738
Abbas Hussain <293289017+AbbasFullstack@users.noreply.github.com>
2026-09-18 18:15:02 +0000

    Add product routes

---
+++ routes/productRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { listProducts, getProduct, createProduct, updateProduct, deleteProduct } = require("../controllers/productController");
+const protect = require("../middleware/auth");
+const adminOnly = require("../middleware/admin");
+
+const router = express.Router();
+
+router.get("/", listProducts);
+router.get("/:id", getProduct);
+router.post("/", protect, adminOnly, createProduct);
+router.put("/:id", protect, adminOnly, updateProduct);
+router.delete("/:id", protect, adminOnly, deleteProduct);
+
+module.exports = router;
